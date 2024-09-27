82d03b3b60c4996236d01b0f82d2f8fb0aca6b80
Steve Carvajal <javier_zam1998@live.com>
2024-09-27 12:47:46 -0500

    Eventos de foco y mouse

--- src/graficos/EventosRaton.java
+++ src/graficos/EventosRaton.java
@@ -3,6 +3,7 @@ package graficos;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
 
@@ -24,15 +25,40 @@ class MarcoRaton extends JFrame{
 		
 		setBounds(700, 300, 600, 450);
 		
-		addMouseListener(new EventoDeRaton());
+		addMouseMotionListener(new EventoDeRaton());
 	}
 }
 
-class EventoDeRaton extends MouseAdapter { // implements MouseListener{
-
-	public void mouseClicked(MouseEvent e) {
-		System.out.println("Se dio click MouseAdapter");
+class EventoDeRaton implements MouseMotionListener{
+	
+	public void mouseDragged(MouseEvent e) {
+		System.out.println("Estas arrastrando un elemento");
 	}
+	
+	public void mouseMoved(MouseEvent e) {
+		System.out.println("Estas moviendo el puntero");
+	}
+}
+
+/*
+class EventoDeRaton extends MouseAdapter { // implements MouseListener{
+	
+	public void mousePressed(MouseEvent e) {
+		//System.out.println(e.getModifiersEx());
+		
+		if(e.getModifiersEx() == MouseEvent.BUTTON1_DOWN_MASK) 
+			System.out.println("Has pulsado el botón izquierdo");
+		else if(e.getModifiersEx() == MouseEvent.BUTTON2_DOWN_MASK)
+			System.out.println("Has pulsado la rueda del ratón");
+		else if(e.getModifiersEx() == MouseEvent.BUTTON3_DOWN_MASK)
+			System.out.println("Has pulsado el botón derecho");
+	}
+
+	/*
+	public void mouseClicked(MouseEvent e) {
+		//System.out.println("Coordenada X: " + e.getX() + " coordenada Y: " + e.getY());
+		System.out.println(e.getClickCount());
+	}*/
 	
 	/*
 	@Override
@@ -63,6 +89,6 @@ class EventoDeRaton extends MouseAdapter { // implements MouseListener{
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("Acabas de salir");
-	}
-	*/
-}
+	}*/
+//}
+
