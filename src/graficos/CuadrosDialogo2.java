c997487e8a9f75cfabe0c7d6bf05c4e87db4ba4f
Steve Carvajal <javier_zam1998@live.com>
2024-10-30 08:59:25 -0500

    Finished funcs lack of design

--- src/graficos/CuadrosDialogo2.java
+++ src/graficos/CuadrosDialogo2.java
@@ -24,17 +24,27 @@ class MarcoCuadrosDialogo2 extends JFrame{
 	
 	public MarcoCuadrosDialogo2() {
 		
-		setBounds(250, 250, 600, 500);
+		setBounds(250, 250, 750, 550);
 		
 		setTitle("Prueba de dialogos");
 		
 		setLayout(new BorderLayout());
 				
 		Box fila1 = Box.createHorizontalBox();			
+		Box fila2 = Box.createHorizontalBox();
 		
+		//CREACIÓN DE Columnas
 		columna1 = Box.createVerticalBox();
 		
 		columna2 = Box.createVerticalBox();
+		
+		columna3 = Box.createVerticalBox();
+		
+		columna4 = Box.createVerticalBox();
+		
+		columna5 = Box.createVerticalBox();
+		
+		columna6 = Box.createVerticalBox();
 
 		initClases();
 		
@@ -60,12 +70,58 @@ class MarcoCuadrosDialogo2 extends JFrame{
 		CrearOpciones(tipo_mensaje, "PLAIN_MESSAGE", JOptionPane.PLAIN_MESSAGE, t_m_plain_message, columna2, false, 2);
 		//----------------------------------------------
 		
+		// Caja de opciones tipo de dialogo
+		CrearOpciones(mensaje, "Cadena", 1, t_cadena, columna3, true, 3);
+		
+		CrearOpciones(mensaje, "Icono", 2, t_icono, columna3, false, 3);
+		
+		CrearOpciones(mensaje, "Componente", 3, t_componente, columna3, false, 3);
+		
+		CrearOpciones(mensaje, "Otros", 4, t_otros, columna3, false, 3);
+		
+		CrearOpciones(mensaje, "Object[]", 5, t_objeto, columna3, false, 3);
+		//----------------------------------------------
+		
+		// Caja de opciones tipo de dialogo		
+		CrearOpciones(confirmar, "DEFAULT_OPTION", JOptionPane.DEFAULT_OPTION, t_default, columna4, true, 4);
+		
+		CrearOpciones(confirmar, "YES_NO_OPTION", JOptionPane.YES_NO_OPTION, t_yes_no, columna4, false, 4);
+		
+		CrearOpciones(confirmar, "YES_NO_CANCEL_OPTION", JOptionPane.YES_NO_CANCEL_OPTION, t_yes_no_cancel, columna4, false, 4);
+		
+		CrearOpciones(confirmar, "OK_CANCEL_OPTION", JOptionPane.OK_CANCEL_OPTION, t_ok_cancel, columna4, false, 4);
+		//----------------------------------------------
+		
+		// Caja de opciones opciones
+		CrearOpciones(opcion, "String[]", 1, t_string_arr, columna5, true, 5);
+		
+		CrearOpciones(opcion, "Icon[]", 2, t_icon_arr, columna5, false, 5);
+		
+		CrearOpciones(opcion, "Object[]", 3, t_object_arr, columna5, false, 5);
+		//----------------------------------------------
+		
+		// Caja de opciones entrada
+		CrearOpciones(entrada, "Campo de texto", 1, t_campo_texto, columna6, true, 6);
+		
+		CrearOpciones(entrada, "Combo", 2, t_combo, columna6, false, 6);
+	
+		//----------------------------------------------
 		fila1.add(columna1);
 		
 		fila1.add(columna2);
 		
-		add(fila1, BorderLayout.CENTER);
+		fila1.add(columna3);
 		
+		fila2.add(columna4);
+		
+		fila2.add(columna5);
+		
+		fila2.add(columna6);
+		
+		fila1.add(fila2);
+		
+		add(fila1, BorderLayout.CENTER);
+				
 		JButton mostrar = new JButton("Mostrar");
 		
 		mostrar.addActionListener(new MostrarDialogoListener());
@@ -90,7 +146,7 @@ class MarcoCuadrosDialogo2 extends JFrame{
 					v_tipo_mensaje = opcion;
 				break;
 			case 3:
-				if(radio.isSelected())
+				if(radio.isSelected())						
 					v_mensaje = opcion;
 				break;
 			case 4:
@@ -149,6 +205,10 @@ class MarcoCuadrosDialogo2 extends JFrame{
 		//INSTANCIA GRUPO TIPO
 		tipo = new ButtonGroup();
 		tipo_mensaje = new ButtonGroup();
+		mensaje = new ButtonGroup();
+		confirmar = new ButtonGroup();
+		opcion = new ButtonGroup();
+		entrada = new ButtonGroup();
 		
 		//INSTANCIA 1ER GRUPO
 		t_r_mensaje = new JRadioButton();		
@@ -162,7 +222,28 @@ class MarcoCuadrosDialogo2 extends JFrame{
 		t_m_warning_message = new JRadioButton();
 		t_m_question_message = new JRadioButton();
 		t_m_plain_message = new JRadioButton();
-				
+		
+		//INSTANCIA 3ER GRUPO 
+		t_cadena = new JRadioButton();
+		t_icono = new JRadioButton();
+		t_componente = new JRadioButton();
+		t_otros = new JRadioButton();
+		t_objeto = new JRadioButton();
+		
+		//INSTANCIA 4TO GRUPO
+		t_default = new JRadioButton();
+		t_yes_no = new JRadioButton();
+		t_yes_no_cancel = new JRadioButton();
+		t_ok_cancel = new JRadioButton();
+		
+		//INSTANCIA 5TO GRUPO
+		t_string_arr = new JRadioButton();
+		t_icon_arr = new JRadioButton();
+		t_object_arr = new JRadioButton();	
+		
+		//INSTANCIA 6TO GRUPO
+		t_campo_texto = new JRadioButton();
+		t_combo = new JRadioButton();
 	}
 	
 	
@@ -172,18 +253,62 @@ class MarcoCuadrosDialogo2 extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub			
 			
-			switch(v_tipo_dialogo) {
+			switch(v_mensaje) {
 				case 1:
-					JOptionPane.showMessageDialog(MarcoCuadrosDialogo2.this, "Mensaje de prueba", "Prueba", v_tipo_mensaje);
+					mensajeObject = "Mensaje de prueba";
 					break;
 				case 2:
-					JOptionPane.showConfirmDialog(MarcoCuadrosDialogo2.this, "Confirmar dialogo");
+					mensajeObject = new ImageIcon("src/graficos/volcan.jpg");
 					break;
 				case 3:
-					JOptionPane.showOptionDialog(MarcoCuadrosDialogo2.this, "Elige", "V de opciones", 1, v_tipo_mensaje, null, null, null);
+					mensajeObject = new JButton("Boton");
 					break;
 				case 4:
-					JOptionPane.showInputDialog(MarcoCuadrosDialogo2.this, "Introduce nombre", "Formulario", v_tipo_mensaje);
+					mensajeObject = new JTextField(10);
+					break;
+				case 5:
+					String[] meses = {"Enero", "Febrero", "Marzo"};
+					mensajeObject = meses;
+					break;
+			}
+			
+			switch (v_opcion) {
+				case 1: 
+					String[] meses = {"Enero", "Febrero", "Marzo"};
+					opcionObject = meses;
+					break;
+				case 2:
+					Icon[] iconos = { new ImageIcon("src/graficos/rojo.png"), new ImageIcon("src/graficos/azul.png"), new ImageIcon("src/graficos/volcan.jpg") };
+					opcionObject = iconos;
+					break;
+				case 3:
+					JRadioButton[] botones = {new JRadioButton("Boton1"), new JRadioButton("Boton2"), new JRadioButton("Boton3")};
+					opcionObject = botones;
+					break;
+			}
+			
+			switch (v_entrada) {
+				case 1:
+					entradaObject = null;
+					break;
+				case 2:
+					String[] meses = {"Enero", "Febrero", "Marzo"};
+					entradaObject = meses;
+					break;
+			}
+			
+			switch(v_tipo_dialogo) {
+				case 1:
+					JOptionPane.showMessageDialog(MarcoCuadrosDialogo2.this, mensajeObject, "Prueba", v_tipo_mensaje);
+					break;
+				case 2:
+					JOptionPane.showConfirmDialog(MarcoCuadrosDialogo2.this, "Confirmar dialogo", "Prueba", v_confirmar);
+					break;
+				case 3:
+					JOptionPane.showOptionDialog(MarcoCuadrosDialogo2.this, "Elige", "V de opciones", JOptionPane.DEFAULT_OPTION, v_tipo_mensaje, null, opcionObject, null);
+					break;
+				case 4:
+					JOptionPane.showInputDialog(MarcoCuadrosDialogo2.this, "Introduce nombre", "Formulario", v_tipo_mensaje, null, entradaObject, null);
 					break;
 			}
 		}
@@ -191,6 +316,7 @@ class MarcoCuadrosDialogo2 extends JFrame{
 	}
 		
 	
+	//Grupo de botones
 	private ButtonGroup tipo, tipo_mensaje, mensaje, confirmar, opcion, entrada;
 	
 	//tipo
@@ -199,9 +325,25 @@ class MarcoCuadrosDialogo2 extends JFrame{
 	//tipo de mensaje
 	private JRadioButton t_m_error_message, t_m_information_message, t_m_warning_message, t_m_question_message, t_m_plain_message;
 	
-	private Box columna1, columna2, columna3;
+	//mensaje 
+	private JRadioButton t_cadena, t_icono, t_componente, t_otros, t_objeto;
+	
+	//Confirmar
+	private JRadioButton t_default, t_yes_no, t_yes_no_cancel, t_ok_cancel;
+	
+	//Opcion
+	private JRadioButton t_string_arr, t_icon_arr, t_object_arr;
+	
+	//Entrada
+	private JRadioButton t_campo_texto, t_combo;
+	
+	private Box columna1, columna2, columna3, columna4, columna5, columna6;
 	
 	private int v_tipo_dialogo, v_tipo_mensaje, v_mensaje, v_confirmar, v_opcion, v_entrada;
+	
+	private Object mensajeObject;
+	
+	private Object[] opcionObject, entradaObject;
 }
 
 
